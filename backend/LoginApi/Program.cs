d2af4509abb6726b88ddbc4b3203fbf22c262248
DINESH KUMAR <abhikumar45444@gmail.com>
2026-08-23 23:05:11 +0530

    Created the first migration

--- backend/LoginApi/Program.cs
+++ backend/LoginApi/Program.cs
@@ -1,9 +1,18 @@
+using LoginApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseMySQL(
+        builder.Configuration.GetConnectionString("DefaultConnection")!
+    )
+);
 
 var app = builder.Build();
 
